02a756d9469b3c1f43fdc580b44a9892e7e5141c
aasimdev <aasimdev69@gmail.com>
2023-06-14 16:49:30 +0500

    changes

--- investors.php
+++ investors.php
@@ -29,7 +29,7 @@ include('header.php'); ?>
             <div class="col-lg-6">
                 <div class="invest-text">
                     <p>We have recently launched an SEC qualified $10 million offering at StartEngine, one of the largest online crowdfunding platforms.</p>
-                    <p>The offering is available to the general public, with a minimum investment of $500.00.  If you are interested in helping us beat cancer, please invest now at: <br> <a href="http://startengine.com/cancervax" target="_blank" rel="noopener noreferrer">http://startengine.com/cancervax</a></p>
+                    <p>The offering is available to the general public, with a minimum investment of $500.00.  If you are interested in helping us beat cancer, please invest now at: <br> <a href="https://www.startengine.com/offering/cancervax" target="_blank">Invest Now</a></p>
                 </div>
             </div>
         </div>
