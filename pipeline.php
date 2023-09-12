321ceccb2dd5a8142b6a498cbcc27867bc4c5f70
aasimdev <aasimdev69@gmail.com>
2023-09-13 03:21:44 +0500

    updates

--- pipeline.php
+++ pipeline.php
@@ -44,16 +44,16 @@ include('header.php'); ?>
         <div class="pC-pipeline-item" id="children-cancer">
             <h4>Children’s Cancer</h4>
             <p>We are developing a disease-specific immunotherapy targeted at treating Ewing Sarcoma, a rare but deadly bone and soft tissue cancer that affects children and young adults. There are currently no treatment options available for recurrence of Ewing Sarcoma. This research effort is led by a team of cancer researchers and physicians who run the Ewing Sarcoma Program at the UCLA Jonsson Comprehensive Cancer Center. </p>
-       
+
             <div class="row align-items-center mt-sm-4 mt-3 mb-0">
                 <div class="col-lg-7">
-                <h5 class="">Facts:</h5>
+                    <h5 class="">Facts:</h5>
                     <ul class="box-list">
                         <li>It is the 2nd most common type of bone cancer in children </li>
                         <li>About 200 children and young adults are diagnosed each year in the United States </li>
                         <li>Survival rate of 55-65% if the disease is localized when diagnosed </li>
                         <li>Survival rate of 20-35% if the disease has spread when diagnosed </li>
-                        <li>No treatment options are currently available for recurrence, <span>meaning 100% death rate</span>  </li>
+                        <li>No treatment options are currently available for recurrence, <span>meaning 100% death rate</span> </li>
                     </ul>
                 </div>
                 <div class="col-lg-5">
@@ -74,15 +74,19 @@ include('header.php'); ?>
                 <div class="col-lg-5">
                     <div class="pC-pipeline-img">
                         <img src="./assets/img/ucv.jpg" alt="ucva">
+                        <a href="http://www.youtube.com/watch?v=zbnhGW5Zz4c" class="theme-btn text-uppercase popup-youtube">
+                            <i class="far fa-play-circle"></i>
+                            <span>Watch video</span>
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
         <div class="pC-pipeline-item" id="uctcp">
             <h4>Universal CAR-T Cell Platform </h4>
-            <p>Based on the foundational nanoparticle technology developed for UCV, we are developing a novel Universal CAR-T Cell therapy, to be delivered as a shot, that can reprogram natural immune T-cells inside the body to seek and destroy any targeted cancer cells.  
+            <p>Based on the foundational nanoparticle technology developed for UCV, we are developing a novel Universal CAR-T Cell therapy, to be delivered as a shot, that can reprogram natural immune T-cells inside the body to seek and destroy any targeted cancer cells.
                 <br><br>
-                CAR-T cell therapy is a type of FDA-approved immunotherapy in which a patient’s T-cells are genetically modified in the laboratory so they will bind to specific proteins (antigens) found on cancer cells and kill them. These modified T-cells are called Chimeric Antigen Receptor (CAR) T Cells. Conventional CAR-T Cell therapy can cost more than <span>$500,000 per dose</span> due to complicated and expensive lab processes. We believe we can dramatically <span>lower the cost to a few thousand dollars per dose</span> by simply helping the body make its own CAR T-cells. 
+                CAR-T cell therapy is a type of FDA-approved immunotherapy in which a patient’s T-cells are genetically modified in the laboratory so they will bind to specific proteins (antigens) found on cancer cells and kill them. These modified T-cells are called Chimeric Antigen Receptor (CAR) T Cells. Conventional CAR-T Cell therapy can cost more than <span>$500,000 per dose</span> due to complicated and expensive lab processes. We believe we can dramatically <span>lower the cost to a few thousand dollars per dose</span> by simply helping the body make its own CAR T-cells.
             </p>
 
             <div class="pC-pipeline-c">
@@ -91,18 +95,18 @@ include('header.php'); ?>
                         <div class="pC-pipeline-text">
                             <h6>Conventional CAR-T Cell Therapy Steps:</h6>
                             <ul class="count-list">
-                                <li>A patient’s T cells are removed from their blood.   </li>
-                                <li> The gene for a special receptor called chimeric antigen receptor (CAR) is inserted into the T cells in the laboratory by gene editing. The gene forces the T cell to express a protein (receptor) on its surface that binds to the cancer cell’s surface protein (antigen).  </li>
-                                <li>Millions of CAR T cells are grown in the laboratory.  </li>
-                                <li> They are then given to the patient by intravenous infusion.  </li>
+                                <li>A patient’s T cells are removed from their blood. </li>
+                                <li> The gene for a special receptor called chimeric antigen receptor (CAR) is inserted into the T cells in the laboratory by gene editing. The gene forces the T cell to express a protein (receptor) on its surface that binds to the cancer cell’s surface protein (antigen). </li>
+                                <li>Millions of CAR T cells are grown in the laboratory. </li>
+                                <li> They are then given to the patient by intravenous infusion. </li>
                                 <li>The CAR protein on the surface of the CAR T cells bind to antigens on the cancer cells and kill them. </li>
                             </ul>
 
                             <h6>CancerVAX Envisioned CAR-T Cells Steps:</h6>
                             <ul class="count-list">
-                                <li>Genetic codes for targeting specific proteins on cancer cells are loaded into a smart nanoparticle using an innovative high-speed, low-cost manufacturing process.   </li>
+                                <li>Genetic codes for targeting specific proteins on cancer cells are loaded into a smart nanoparticle using an innovative high-speed, low-cost manufacturing process. </li>
                                 <li>
-                                Millions of nanoparticles are injected into the patient as a shot. The nanoparticles attach to the patient’s natural T-cells inside the body, unload the genetic code, and reprogram the T-cells to target specific cancer cells and kill them.    </li>
+                                    Millions of nanoparticles are injected into the patient as a shot. The nanoparticles attach to the patient’s natural T-cells inside the body, unload the genetic code, and reprogram the T-cells to target specific cancer cells and kill them. </li>
                             </ul>
                         </div>
                     </div>
