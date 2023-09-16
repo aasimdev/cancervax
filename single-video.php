0d8e17b06a613120f7f00bd8464fe74aa65e4341
aasimdev <aasimdev69@gmail.com>
2023-09-17 01:46:21 +0500

    update ezra video

--- single-video.php
+++ single-video.php
@@ -8,9 +8,9 @@ include('header.php'); ?>
 <section class="podcast-detail">
     <div class="container">
             <div class="podcast-detail-wrap">
-                <h1>September 15, 2023 - Ezra Cohen - Tempus Labs</h1>
+                <h1>September 16, 2023 - Ezra Cohen - Tempus Labs</h1>
                 <div class="podcast-video">
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/8GYPZ-nhnGc?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/Ez4gXwP56Xo?autoplay=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </div>
             </div>
     </div>
