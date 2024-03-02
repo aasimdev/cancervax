f68674b8a0b1c010b53971bf1ce5831f401c3233
aasimdev <aasimdev69@gmail.com>
2024-03-02 05:11:23 +0500

    updated gtag manager

--- header.php
+++ header.php
@@ -95,10 +95,31 @@
     }
     ?>
 
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-K6D95QZP');
+    </script>
+    <!-- End Google Tag Manager -->
+
 </head>
 
 <body>
-
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K6D95QZP" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <?php
     $page_name = basename($_SERVER['PHP_SELF']);
     if ($page_name != "index.php" && $page_name != "" && $page_name != "/") {
