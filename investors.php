5481fc6d05f75f42fda30af396c9127bab7dbd46
aasimdev <aasimdev69@gmail.com>
2024-04-20 03:06:51 +0500

    added news video on investor page

--- investors.php
+++ investors.php
@@ -170,7 +170,7 @@ include "header.php";
 
                                     <div class="learnImg first-mobile"> <img src="./assets/img/girl_cancervax.jpg" alt="hero" class="img-fluid-ca">
                                     </div>
-                              
+
 
                                     <p>We have created our first cancer drug candidate – a single-disease specific immunotherapy targeting Ewing sarcoma, a rare but deadly bone and soft tissue cancer primarily affecting children and young adults. Unfortunately, the death rate for patients with recurrent Ewing sarcoma is nearly 100%. ​
                                     </p>
@@ -418,7 +418,15 @@ include "header.php";
                                 </table>
                             </div>
                             <h4 id="updates">Updates</h4>
-                            <div class="placeholder-he"></div>
+                            <div class="cchat mt-5">
+                                    <div class="cchat-box pb-4" data-video="trend">
+                                        <a class="popup-youtube" href="https://www.youtube.com/watch?v=Bk39recgMGU"></a>
+                                        <div class="cchat-thumbnail thumbnail-overlay h-auto">
+                                            <img src="//img.youtube.com/vi/Bk39recgMGU/maxresdefault.jpg" class="object-fit-contain" alt="Cancervax">
+                                        </div>
+                                        <i class="far fa-play-circle"></i>
+                                    </div>
+                                </div>
                             <h4>Market Statistics</h4>
 
                             <div class="link-placeholder">
