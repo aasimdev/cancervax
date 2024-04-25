1054cb93b9d18785b81273ddb10622ab27ff80bf
aasimdev <aasimdev69@gmail.com>
2024-04-26 03:12:01 +0500

    updated invest page for first term

--- php/investemail.php
+++ php/investemail.php
@@ -5,7 +5,6 @@ include dirname(__FILE__) . '/settings/settings.php';
 require 'phpmailertesting/PHPMailer/class.phpmailer.php';
 
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$emailaddress = strip_tags(trim($_POST["email"]));
 
@@ -17,24 +16,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		}
 
 		echo '<div class="alert notification alert-error">Error:<br><ul>' . $errortext . '</ul></div>';
-		echo '<script>alert("its not working fine"); </script>';
 	} else {
 		$mail = new PHPMailer;
 		$mail->CharSet = "UTF-8";
-		$mail->AddReplyTo($emailaddress);
+		$mail->From = "info@deeppower.com";
+		$mail->FromName = "DeepPower";
+		$mail->AddReplyTo($emailaddress, $guestname);
 		$mail->addAddress('asimhameed11@gmail.com');
-		$mail->Subject = "CancerVax submission";
+		// $mail->Subject  = $guestname + '';
+		$mail->Subject = "Newsletter Form";
 		$mail->IsHTML(true);
 		$mail->Body    = '<html>
 
 		 
 		 <body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
-				 <p><span style="font-weight:bold;font-size:16px;padding-left:10px">New contact:</span> ' . $emailaddress . '</p>
+			<p><span style="font-weight:bold;font-size:16px;padding-left:10px">Subscriber Email:</span> ' . $emailaddress . '</p>	
 		 </body>
 		 </html>';
 		$mail->send();
 		if ($redirectForm == true) {
-			echo '<script>alert("its working fine"); </script>';
+			echo '<script>setTimeout(function () { window.location.replace("/subscriber-thankyou.php") }, 1000); </script>';
 		}
 	}
 }
