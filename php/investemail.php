bb79824b1b56881d81e43343cbf3996c9bb6039d
aasimdev <aasimdev69@gmail.com>
2024-04-26 03:24:10 +0500

    updated invest page for first term

--- php/investemail.php
+++ php/investemail.php
@@ -1,33 +1,43 @@
 <?php
 if (!isset($_SESSION)) session_start();
-if (!$_POST) exit;
+if (!isset($_POST)) exit;
 include dirname(__FILE__) . '/settings/settings.php';
 require 'phpmailertesting/PHPMailer/class.phpmailer.php';
 
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$emailaddress = strip_tags(trim($_POST["email"]));
+    // Initialize $errors array
+    $errors = array();
 
-	if ( isset($_SERVER["OS"]) && $_SERVER["OS"] == "Windows_NT" ) {
-		$hostname = strtolower($_SERVER["COMPUTERNAME"]);
-	} else {
-		$hostname = `hostname`;
-		$hostnamearray = explode('.', $hostname);
-		$hostname = $hostnamearray[0];
-	}
+    $emailaddress = strip_tags(trim($_POST["email"]));
 
-	header("Content-Type: text/plain");
-	header("X-Node: $hostname");
-	$from = "DLOC";
-	$toemail = "zack@digitallocations.com";
-	$subject = "Newsletter Form";
-	$message = '
-	 Email: ' . $emailaddress;
-	$result = mail($toemail, $subject, $message, "From: $from" );
-	if ( $result && $redirectForm == true) {
-		header("Location: /thank-you.php");
-	} else {
-		echo 'FAIL';
-	}
-
+    if ($errors) {
+        // Output errors in a list
+        $errortext = "";
+        foreach ($errors as $error) {
+            $errortext .= '<li>' . $error . "</li>";
+        }
+        echo '<div class="alert notification alert-error">Error:<br><ul>' . $errortext . '</ul></div>';
+    } else {
+        $mail = new PHPMailer;
+        $mail->CharSet = "UTF-8";
+        $mail->AddReplyTo($emailaddress);
+        $mail->addAddress('asimhameed11@gmail.com');
+        $mail->Subject = "CancerVax submission";
+        $mail->IsHTML(true);
+        $mail->Body = '<html>
+            <body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
+                <p><span style="font-weight:bold;font-size:16px;padding-left:10px">New contact:</span> ' . $emailaddress . '</p>
+            </body>
+            </html>';
+        
+        // Check for errors while sending mail
+        if (!$mail->send()) {
+            echo "Mailer Error: " . $mail->ErrorInfo;
+        } else {
+            if ($redirectForm == true) {
+                echo '<script>alert("its working fine"); </script>';
+            }
+        }
+    }
 }
+?>
