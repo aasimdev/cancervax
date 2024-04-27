a981a71bf6441a30f6e6b100a51a0e3ee4326e30
aasimdev <aasimdev69@gmail.com>
2024-04-28 02:00:24 +0500

    updated ode

--- php/investemail.php
+++ php/investemail.php
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();                                            //Send using SMTP
         $mail->Host       = 'send.smtp.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'tariq_admin';                     //SMTP username
+        $mail->Username   = 'zack_cancervax_com';                     //SMTP username
         $mail->Password   = 'c5t0mwft';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 2525;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
