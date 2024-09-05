31306d516a76dedead49ea9df13c51fc5035dea6
aasimdev <aasimdev69@gmail.com>
2024-09-05 13:02:07 +0500

    updated all inner pages color

--- footer.php
+++ footer.php
@@ -126,7 +126,7 @@ if ($page_name !== "prep.php") {
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="teamLabel"></h5>
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body p-0">
 
