e44b71a0aab0e34e17cadecd9181e81828210a40
aasimdev <aasimdev69@gmail.com>
2025-02-02 01:10:56 +0500

    update homepage banner

--- index.php
+++ index.php
@@ -18,12 +18,29 @@ include('header.php'); ?>
     }
 </style>
 
+<section class="coming-soon">
+    <div class="container">
+        <div class="coming-soon-title">Upcoming Webinar </div>
+        <div class="coming-soon-content">Exciting Update on Our
+        <br class="d-none d-md-inline-block" /> Revolutionary Cancer Treatment
+        </div>
+        <p>Sign up now to reserve your spot
+        </p>
+        <form action="https://submit.jotform.com/submit/250306574577058/" method="POST" autocomplete="off">
+            <input type="hidden" name="formID" value="250306574577058">
+            <input type="email" id="webinar-email" name="q3_webinaremail" placeholder="Enter email address" required>
+         
+            <button type="submit">Sign up!</button>
+        </form>
+    </div>
+</section>
+
 
 <section class="banner-main">
     <div class="banner-main-bg banner-home-up">
        
         <div class="container">
-        <div class="banner-main-content text-center mx-auto">
+        <div class="banner-main-content banner-heading text-center mx-auto">
             <h1 style="color: #295d6e">A Revolutionary Way
                 to Fight Cancer</h1>
         </div>
