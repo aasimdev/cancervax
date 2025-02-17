ab4835e0534b830ea50d3d98b02b20664afd8503
aasimdev <aasimdev69@gmail.com>
2025-02-18 03:46:31 +0500

    changes

--- webinar.php
+++ webinar.php
@@ -10,14 +10,6 @@ include('header.php'); ?>
         <div class="coming-soon-content">Exciting Update on Our
         <br class="d-none d-md-inline-block" /> Revolutionary Cancer Treatment
         </div>
-        <p>Sign up now to reserve your spot
-        </p>
-        <form action="https://submit.jotform.com/submit/250306574577058/" method="POST" autocomplete="off">
-            <input type="hidden" name="formID" value="250306574577058">
-            <input type="email" id="webinar-email" name="q3_webinaremail" placeholder="Enter email address" required>
-         
-            <button type="submit">Sign up!</button>
-        </form>
     </div>
 </section>
 
