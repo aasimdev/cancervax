3bb30473b412280f8b39ccbc04299b65076b79c0
aasimdev <aasimdev69@gmail.com>
2025-02-19 01:33:54 +0500

    revert about us

--- about.php
+++ about.php
@@ -115,30 +115,41 @@ include('header.php'); ?>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6 col-xl-4">
-                    <div class="team-box">
-                        <div class="team-box-img">
-                            <a href="#">
-                                <img src="./assets/img/sumant-ramachandra.png" class="object-fit-cover" style="object-position: 100% 0;" alt="john">
-                            </a>
-                        </div>
-                        <h5>
-                            Sumant Ramachandra, <span>MD, PhD, MBA</span>
-                        </h5>
-                        <p>
-                            Chief Scientific Advisor
-                        </p>
-                        <div class="team-description">
-                            <p class="py-2">Sumant Ramachandra serves as a member of the Board Director of Lyell Immunopharma (“Lyell”), following Lyell’s acquisition of ImmPACT Bio (“ImmPACT”) in October 2024. As CEO of ImmPACT from November 2021 until its acquisition, he led capital raises, secured clinical grants, and built a team that achieved FDA clearance for three Investigational New Drug (IND) applications, established manufacturing capabilities, and advanced multiple clinical studies. </p>
-                            <p class="py-2">Previously, Dr. Ramachandra served as Chief Science, Technology, and Medical Officer at Baxter International, starting in June 2017. He was also appointed President of Baxter Pharmaceuticals in 2019 and chaired Baxter’s Global Inclusion Council, focusing on diversity and inclusion initiatives. Prior to Baxter, he held the role of Senior Vice President, Head of Research & Development, at Pfizer Essential Health, and served as Chief Scientific Officer at Hospira from 2008 until Pfizer acquired Hospira in 2015. Earlier in his career, Dr. Ramachandra held senior roles at Pfizer and Merck & Co., specializing in oncology, global product development, medical affairs, and business development, as well as clinical pharmacology. He began his medical career as an intern and resident physician at Massachusetts General Hospital, Harvard Medical School.</p>
-                            <p class="py-2">Dr. Ramachandra earned his undergraduate degree in biochemistry, a PhD in experimental pathology (with a focus on chronic lymphocytic leukemia), and an MD from Rutgers University. He also holds an MBA from the Wharton School, University of Pennsylvania.</p>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
         <div class="team-wrapper">
             <div class="row my-4 justify-content-center">
+                <div class="col-6 col-xl-4">
+                    <div class="team-box">
+                        <div class="team-box-img">
+                            <a href="#">
+                                <img src="./assets/img/lakey.png" class="object-fit-cover" style="    object-position: bottom;" alt="john">
+                            </a>
+                        </div>
+                        <h5>
+                            Jonathan Lakey, <span>PhD</span>
+                        </h5>
+                        <p>
+                            Scientific Advisor
+                        </p>
+                        <div class="team-description">
+                            <ul class="theme-list pb-4 px-4">
+                                <li>An inventor of the famous “Edmonton Protocol” for treating Type 1 diabetes</li>
+                                <li>Extensive experience in the fields of oncology, stem cells, and organ transplantation</li>
+                                <li>Currently Professor Emeritus at University of California, Irvine.</li>
+                                <li>Formerly Postdoctoral Fellow, Cryobiology Research Institute Indiana University Indianapolis
+                                </li>
+                                <li> Formerly Postdoctoral Fellow, Clinical tissue banking University of Washington</li>
+                                <li>Formerly Postdoctoral Fellow, Tissue banking/cryopreservation University of Alberta</li>
+                                <li>Published over 495 scientific papers, 45 books, and over 500 scientific abstracts</li>
+                                <li>Advisor to many biotech companies</li>
+                                <li>Received his BS in Zoology/Animal Biology from University of Alberta</li>
+                                <li>Received his MS in Zoology/Animal Biology from University of Alberta</li>
+                                <li>Received his PhD of Islet transplantation, diabetes, and medical devices from University of Alberta</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
                 <div class="col-6 col-xl-4">
                     <div class="team-box">
                         <div class="team-box-img">
@@ -168,37 +179,7 @@ include('header.php'); ?>
                         </div>
                     </div>
                 </div>
-                <div class="col-6 col-xl-4">
-                    <div class="team-box">
-                        <div class="team-box-img">
-                            <a href="#">
-                                <img src="./assets/img/lakey.png" class="object-fit-cover" style="    object-position: bottom;" alt="john">
-                            </a>
-                        </div>
-                        <h5>
-                            Jonathan Lakey, <span>PhD</span>
-                        </h5>
-                        <p>
-                             Scientific Advisor
-                        </p>
-                        <div class="team-description">
-                            <ul class="theme-list pb-4 px-4">
-                                <li>An inventor of the famous “Edmonton Protocol” for treating Type 1 diabetes</li>
-                                <li>Extensive experience in the fields of oncology, stem cells, and organ transplantation</li>
-                                <li>Currently Professor Emeritus at University of California, Irvine.</li>
-                                <li>Formerly Postdoctoral Fellow, Cryobiology Research Institute Indiana University Indianapolis
-                                </li>
-                                <li> Formerly Postdoctoral Fellow, Clinical tissue banking University of Washington</li>
-                                <li>Formerly Postdoctoral Fellow, Tissue banking/cryopreservation University of Alberta</li>
-                                <li>Published over 495 scientific papers, 45 books, and over 500 scientific abstracts</li>
-                                <li>Advisor to many biotech companies</li>
-                                <li>Received his BS in Zoology/Animal Biology from University of Alberta</li>
-                                <li>Received his MS in Zoology/Animal Biology from University of Alberta</li>
-                                <li>Received his PhD of Islet transplantation, diabetes, and medical devices from University of Alberta</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+
                 <div class="col-md-6 col-xl-4">
                     <div class="team-box">
                         <div class="team-box-img">
@@ -226,25 +207,6 @@ include('header.php'); ?>
                                 <li>Received a B.A. degree from the Johns Hopkins University</li>
                                 <li>Received his M.D. degree from Stanford University</li>
                             </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-xl-4">
-                    <div class="team-box">
-                        <div class="team-box-img">
-                            <a href="#">
-                                <img src="./assets/img/steven.png" class="object-fit-cover" style="object-position: 100% 0;" alt="john">
-                            </a>
-                        </div>
-                        <h5>
-                            Steven T. Rosen, <span>MD</span>
-                        </h5>
-                        <p>
-                            Scientific Advisor
-                        </p>
-                        <div class="team-description">
-                            <p class="py-2">Steven T. Rosen, MD, FACP, FASCO, FAIME, is the Executive Vice President, Director Emeritus of Beckman Research Institute & City of Hope, Professor, Hematology and Hematopoietic Cell Transplantation, Schwartz Endowed Professorship in Duarte, California. Following his graduation with distinction from Northwestern University Medical School’s Six-Year Honors Program in Chicago, Illinois, Dr. Rosen completed his residency in internal medicine at Northwestern and a fellowship in medical oncology at the National Cancer Institute in Maryland. He served as the Director of the Robert H. Lurie Comprehensive Cancer Center at Northwestern University Feinberg School of Medicine from 1989-2014.</p>
-                            <p class="py-2">Dr. Rosen’s laboratory research focuses on experimental therapeutics and hematologic malignancies. He has received funding from the National Cancer Institute (NCI), American Cancer Society, Leukemia and Lymphoma Society (LLS), Multiple Myeloma Research Foundation, Department of Defense and Rising Tide Foundation. Dr. Rosen has received numerous grant awards and contracts and has published more than 500 scientific papers. He is editor-in-chief of Cancer Treatment and Research. Dr. Rosen was the chair of the Medical Science Committee of the LLS and served on its Board. He also serves on the Board of American Society of Clinical Oncology’s Conquer Cancer Foundation. Dr. Rosen has been an advisor to more than two dozen NCI Comprehensive Cancer Centers. </p>
                         </div>
                     </div>
                 </div>
@@ -280,7 +242,7 @@ include('header.php'); ?>
         </div>
         <div class="team-wrapper">
             <div class="row my-4 justify-content-center">
-             
+
             </div>
         </div>
     </div>
