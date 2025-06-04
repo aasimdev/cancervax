4e46fe9167bdf425d17e590084f7a6b454a1effe
aasimdev <aasimdev69@gmail.com>
2025-06-05 01:54:46 +0500

    Add new article detailing CancerVax's achievement in creating a cell-targeting nanoparticle, highlighting its innovative approach and future steps in cancer treatment. Update news archive and main news page accordingly.

---
+++ cancerVax-achieves-major-milestone-with-successful-creation-of-cell-targeting-nanoparticle.php
@@ -0,0 +1,48 @@
+<?php
+$GLOBALS['title'] = "CancerVax Achieves Major Milestone with Successful Creation of Cell Targeting Nanoparticle - CancerVax";
+$GLOBALS['desc'] = "";
+$GLOBALS['keywords'] = "";
+include('header.php'); ?>
+<section class="news-detail">
+    <div class="container">
+        <div class="post-title">
+            <h1>CancerVax Achieves Major Milestone with Successful Creation of Cell Targeting Nanoparticle </h1>
+        </div>
+
+        <h3>Recent independent analyses confirmed that the Company has successfully created a ligand conjugated nanoparticle that can target specific biomarkers on the surface of cancer cells</h3>
+        <p>Lehi, Utah – June 5, 2025 – CancerVax, Inc., the developer of a breakthrough universal cancer treatment (UCT) platform designed to use the body’s immune system to treat cancer, today announced the successful creation of its nanoparticle that can target specific cells that are potentially cancerous.  </p>
+
+        <p>The Company’s revolutionary approach detects, and universally marks, cancer cells as viruses, such as measles. This tricks the immune system into killing cancer cells as if they were known viruses. To specifically elicit an immune response against cancer cells, CancerVax technology uses a precision 2-step cancer cell detection mechanism in highly functionalized lipid nanoparticles.   </p>
+
+        <p>Step 1: The nanoparticle latches onto surface proteins highly expressed on cancer cells (“Marker1”). Step 2: The nanoparticle releases a proprietary Smart mRNA payload that is conditionally activated based on cancer genetic signatures (“Marker2”). When the Smart mRNA encounters a cell with the matching cancer-specific signature, it forces the cell to produce viral proteins, which disguises the cancer cell as a virus, such as measles. This makes the cancer cells recognizable to the immune system and induces immune cells to attack and kill the cancer cells. When the Smart mRNA encounters a healthy cell signature, it will not turn on, leaving the healthy cell unharmed. This two-step, on/off cancer signature detection is highly targeted to the cancer cell and is believed to improve the safety and efficacy of its treatments.   </p>
+
+        <p>With the help of one of its world-class development partners, the Company successfully attached Marker1 molecules onto the surface of nanoparticles with high efficiency. These Marker1 conjugated nanoparticles can then seek out and deliver Smart mRNAs to cancer cells with the matching surface protein.</p>
+
+        <p>Dr. George Katibah, Chief Scientific Officer of CancerVax said, “We anticipate that our targeted nanoparticles will improve delivery of our Smart mRNAs to cancer cells while significantly reducing uptake by healthy cells. This is potentially a game changer for increasing efficacy and reducing toxicity.” </p>
+
+        <p>Recent positive lab confirmation demonstrated strong conjugation efficiency, which indicates that our nanoparticles can be manufactured at scale. Additional studies are being conducted to further increase efficiency rates even in preparation for clinical-grade production.
+        </p>
+
+        <p>Dr. Katibah commented further, “This is an important milestone for us. In February 2025, we proved that our Smart mRNA containing Marker2 worked. However, we needed a Marker1 nanoparticle to complete our innovative 2-step detection strategy. Now, we have it. These exciting results provide our team with high levels of confidence that we can successfully produce fully functional nanoparticles that can latch on to cancer cells and deliver Smart mRNAs to disguise cancer as a common disease. This is a significant, positive step towards our goal of developing a better way to treat cancer.”</p> 
+        <p>Dr. Adam Grant, Principal Scientist of CancerVax said, “These results are very exciting and confirm that the technology we have designed can be manufactured with limited issues. We evaluated a lot of data to give us the best chance of success, but the rubber meets the road only when we are able to validate our findings in well-designed experiments. I’m excited that what we’re seeing in the data is consistent with the expected results we hypothesized. These successful lab reports confirm that we are on the right path!” </p>  
+        <p>The Company’s next step is to combine Marker2 Smart mRNA payloads into Marker1 nanoparticles and create full therapeutic nanoparticles for further in-vitro optimization, and in-vivo animal testing for efficacy and toxicity. </p>    
+
+        <h3>About Cancervax</h3>
+
+        <p>CancerVax, Inc. is a pre-clinical biotechnology company developing a novel Universal Cancer Treatment platform that will be customizable, as an injection, to treat many types of cancer. Our innovative approach DETECTS, MARKS, and KILLS only cancer cells. By making cancer cells “look” like well immunized common diseases such as measles or chickenpox, we intend to use the body’s natural immune system to easily kill the cancer cells. We have also created our first cancer drug candidate – a single-disease specific immunotherapy targeting Ewing sarcoma, a rare but deadly bone and soft tissue cancer primarily affecting children and young adults. We look forward to the day when treating cancer will be as simple as getting a shot – a better way to treat cancer. To learn more, please visit  <a href="www.cancervax.com" target="_blank" rel="noopener noreferrer">www.cancervax.com</a> </p>
+
+        <h3>Forward-Looking Statements </h3>
+
+        <p>This press release may contain “forward-looking statements” within the meaning of the safe harbor provisions of the U.S. Private Securities Litigation Reform Act of 1995. Forward-looking statements are neither historical facts nor assurances of future performance. Instead, they are based only on our current beliefs, expectations, and assumptions regarding the future of our business, plans and strategies, projections, anticipated events and trends, the economy, and other future conditions. Because forward-looking statements relate to the future, they are subject to inherent uncertainties, risks, and changes in circumstances that are difficult to predict, many of which are outside our control. Our actual results and financial condition may differ materially from those in the forward-looking statements. Therefore, you should not rely on any of these forward-looking statements. Any forward-looking statement made by us in this release is based only on information currently available to us and speaks only as of the date it is made. We undertake no obligation to publicly update any forward-looking statement, whether written or oral, that may be made from time to time, whether as a result of new information, future developments, or otherwise.</p>
+
+        <p>Press Contact: <br>
+        Cancervax, Inc. <br>
+            Tel: <a href="tel:8053561810">(805) 356-1810</a> <br>
+            <a href="mailto:communications@cancervax.com">communications@cancervax.com</a>
+        </p>
+
+    </div>
+</section>
+
+<?php
+include('footer.php'); ?>
