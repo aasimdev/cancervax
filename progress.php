003d544e75365e450c5b85eb6db066071a5e7a45
aasimdev <aasimdev69@gmail.com>
2025-06-12 02:17:13 +0500

    Update progress.php with new links and milestone details, add two new webinar pages for upcoming events, and include a new lipid nanoparticle image. Adjust existing content for clarity and layout improvements.

--- progress.php
+++ progress.php
@@ -15,34 +15,32 @@ include('header.php'); ?>
         <div class="graph-wrap">
             <img src="./assets/img/graph.png" class="d-none d-md-block" alt="graph">
             <img src="./assets/img/graph-mobile.png" class="d-md-none" alt="graph">
-            <a href="#children-cancer" style="     display: inline-block;
-    position: absolute;
-    left: 0;
-    top: 25%;
-    width: 24%;
-    height: 18.5%;"></a>
-            <a href="#ucvp" style="     display: inline-block;
-    position: absolute;
-    left: 0;
-    top: 44%;
-    width: 24%;
-    height: 26%;"></a>
-            <a href="#uctcp" style="
+          
+            <a href="cancervax-targets-hard-to-treat-pancreatic-cancer.php" style="
     display: inline-block;
     position: absolute;
-    left: 0;
-    top: 70%;
+       left: 2%;
+    top: 68%;
     width: 24%;
-    height: 26%;
+    height: 10%;
+"></a>
+<a href="cancervax-targets-liver-cancers-to-further-showcase-flexibility-of-its-universal-platform.php" style="
+    display: inline-block;
+    position: absolute;
+       left: 2%;
+    top: 82.3%;
+    width: 24%;
+    height: 10%;
 "></a>
         </div>
 
         <ul class="theme-list mt-5 ">
-            <li>Filed multiple patent applications </li>
+            <li>Filed multiple patent applications
+            </li>
             <li> Designed several Smart mRNAs for targeting multiple cancer types​</li>
-            <li> Lab tests successfully validated Smart mRNAs worked in actual cancer cells (Q1 2025)</li>
-            <li> Complete full nanoparticle for pancreatic cancer (Q3 2025)</li>
-            <li> Complete animal studies to determine toxicity and efficacy of pancreatic cancer treatment  (Q1 2026)</li>
+            <li>Lab tests successfully validated Smart mRNAs worked in actual cancer cells (Q1 2025)</li>
+            <li> Complete therapeutic nanoparticles for the above cancer indications (Q3 2025)</li>
+            <li> Complete animal studies to determine toxicity and efficacy of the above cancer treatments (Q1 2026)</li>
         </ul>
     </div>
 </section>
@@ -59,27 +57,75 @@ include('header.php'); ?>
                     <p>Smart mRNA turned <strong class="text-decoration-underline">ON</strong> in <br> cancer cells only
                     </p>
                     <div class="dev-milestone-img">
-                    <img src="./assets/img/fluorescene-microscopy.png?v=<?php echo filemtime('./assets/img/fluorescene-microscopy.png'); ?>" alt="fluorescene-microscopy">
-                    </div>
-                    <div class="text-center mt-5">
-                    <a href="/webinar" class="theme-btn">View Webinar</a>
+                        <img src="./assets/img/fluorescene-microscopy.png?v=<?php echo filemtime('./assets/img/fluorescene-microscopy.png'); ?>" alt="fluorescene-microscopy">
                     </div>
                 </div>
                 <div class="col-lg-5">
                     <p>Smart mRNA turned <strong class="text-decoration-underline">OFF</strong> <br> in healthy cell line (HEK293)</p>
                     <div class="dev-milestone-img">
-                    <img src="./assets/img/gfp-smart-mrnas-cropped.png?v=<?php echo filemtime('./assets/img/gfp-smart-mrnas-cropped.png'); ?>" alt="fluorescene-microscopy">
+                        <img src="./assets/img/gfp-smart-mrnas-cropped.png?v=<?php echo filemtime('./assets/img/gfp-smart-mrnas-cropped.png'); ?>" alt="fluorescene-microscopy">
                     </div>
-                    <div class="text-center mt-5">
-                    <a href="https://cancervax.com/cancervax-announces-successful-tests-of-its-smart-mrna-technology.php" class="theme-btn">View Press Release</a>
-                    </div>
+                    
                 </div>
             </div>
+            <div class="row">
+                    <div class="col-lg-7">
+                      
+                        <div class="text-center mt-5">
+                        <a href="/webinar-feb-25-2025" class="theme-btn">View Webinar</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-5">
+                        <div class="text-center mt-lg-5">
+                        <a href="cancervax-announces-successful-tests-of-its-smart-mrna-technology.php" class="theme-btn">View Press Release</a>
+                        </div>
+                    </div>
+                </div>
         </div>
     </div>
 </section>
 
+<section class="dev-milestones">
+    <div class="container">
+        <div class="dev-milestones-body">
+            <h5>May 31, 2025 – Successfully Made Cell Targeting Nanoparticle!</h5>
+            <div class="dev-white-bg lipid-nanoparticle-wrap">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="lipid-nanoparticle">
+                            <img src="./assets/img/lipid-nanoparticle.png?v=<?php echo filemtime('./assets/img/lipid-nanoparticle.png'); ?>" alt="lipid-nanoparticle">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <ul class="theme-list">
+                            <li>Independent analysis confirmed efficient and successful attachment of Marker1 onto lipid nanoparticles
+                            </li>
+                            <li>Marker1 is a molecule that will seek out  specific proteins on cancer cell surface and deliver our Smart mRNA payload
+                            </li>
+                            <li>Next step, we will put Smart mRNAs inside the nanoparticles to make  complete cancer targeting and killing nanoparticles. </li>
+                        </ul>
+                       
+                    </div>
+                </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                      
+                        <div class="text-center mt-5">
+                            <a href="/webinar-may-31-2025" class="theme-btn">View Webinar</a>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="text-center mt-lg-5">
+                            <a href="cancerVax-achieves-major-milestone-with-successful-creation-of-cell-targeting-nanoparticle.php" class="theme-btn">View Press Release</a>
+                        </div>
+                    </div>
+                </div>
+           
 
+        </div>
+    </div>
+</section>
 
 
 
